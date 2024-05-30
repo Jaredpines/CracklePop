22370225385e13cf57ee3d69e254e6a3dbbd1870
Jaredpines <Pinesjared@gmail.com>
2024-05-30 10:54:54 -0700

    Finished code

---
+++ CracklePop.py
@@ -0,0 +1,13 @@
+def main():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print('CracklePop')
+        elif number % 3 == 0:
+            print("Crackle")
+        elif number % 5 == 0:
+            print("Pop")
+        else:
+            print(number)
+
+
+main()
